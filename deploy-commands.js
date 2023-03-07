e91fc4b91f5ef8d06ad9cdf6fb924886230ae8b2
Clément Moréna <clement.morena@epfl.ch>
2023-03-07 21:48:20 +0100

    deploy commands fixed

--- deploy-commands.js
+++ deploy-commands.js
@@ -13,7 +13,7 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const command = require(`./src/commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 
