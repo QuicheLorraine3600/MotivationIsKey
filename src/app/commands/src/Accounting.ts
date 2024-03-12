9beb804a64745142aaeebbb0dbeafc9c0e6b0a62
Athanaze <sacha.liechti@arsent.ch>
2024-03-12 21:34:43 +0000

    Made with 💕 in the web IDE

---
+++ src/app/commands/src/Accounting.ts
@@ -0,0 +1,53 @@
+import { SlashCommandBuilder, TextChannel } from "discord.js";
+
+import Bot from "../../Bot";
+import { Command, Interaction } from "../Command";
+import { registerNewPendingPayment, AccountingEntry } from "../../modules/AccountingStuff";
+export default class Accounting extends Command {
+	override get data() {
+		return new SlashCommandBuilder()
+			.setName('accountingEntry')
+			.setDescription('Add an accounting entry for the monkey empire')
+			.addUserOption(option =>
+				option.setName("sending_monkey")
+					.setDescription("The monkey that must pay")
+					.setRequired(true)
+			)
+			.addUserOption(option =>
+				option.setName("receiving_monkey")
+					.setDescription("The monkey receiving")
+					.setRequired(true)
+			)
+			.addIntegerOption(option =>
+				option.setName("amount")
+					.setDescription("1-999999 (we prevent int overflow like proper cs monkeys :insert sunglass emoji: ")
+					.setMinValue(1).setMaxValue(999999)
+					.setRequired(true)
+			)
+			.addStringOption(option =>
+				option.setName("unit")
+					.setDescription("Unit of payment: could be bananas, grams of cocaine, barrels of oil...")
+					.setRequired(true)
+			)
+	}
+
+	override async execute(bot: Bot, interaction: Interaction) {
+		const sending_monkey = interaction.options.getUser("sending_monkey", true)
+		const receiving_monkey = interaction.options.getUser("receiving_monkey", true)
+		const amount = interaction.options.getInteger("amount", true)
+		const unit = interaction.options.getString("unit", true)
+		const channel = interaction.channel
+		if (channel instanceof TextChannel) {
+			const entr: AccountingEntry = {
+				sending_monkey:sending_monkey,
+				receiving_monkey:receiving_monkey,
+				amount:amount,
+				unit:unit
+			}
+
+			registerNewPendingPayment(bot, entr)
+
+			interaction.reply({ content: "Accounting entry added !", ephemeral: true })
+		}
+	}
+}
